48494cc9fe1ec22e8595e415b44defc40c9391ba
shernandez33 <shernandezramos33@gmail.com>
2026-01-17 15:45:13 -0400

    Se agrega el archivo main.py con instrucciones para la sincronizacion

---
+++ main.py
@@ -0,0 +1,12 @@
+git remote add origin git@github.com:shernandez-33/sincro_local_remoto.git
+
+se genera la sincronizacion local/remota, siguiendo los pasos a continuacion:
+ 
++ echo "# sincro_local_remoto" >> README.md
++ git init
++ git add README.md
++ git commit -m "first commit"
++ git branch -M main
++ git remote add origin git@github.com:shernandez-33/sincro_local_remoto.git
+
+
